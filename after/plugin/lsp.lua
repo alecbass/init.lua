b0a1f5440a90259787e2b4868c6c9787f99317d5
alecbass <alec.bassingthwaighte@gmail.com>
2023-10-03 18:54:39 +1000

    Installed file tree tooling

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -22,7 +22,6 @@ cmp.setup({
 
 
 lsp.on_attach(function(client, bufnr)
-    print("LSP working")
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
     lsp.default_keymaps({buffer = bufnr})
