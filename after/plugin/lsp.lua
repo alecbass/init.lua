b5ca801c6ffed930d049cb9443d7b64c6c252953
alecbass <alec.bassingthwaighte@gmail.com>
2025-07-02 19:01:29 +1000

    Remove inline for Supermaven and add it to nvim-cmp instead

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,36 +1,7 @@
 local lspconfig = require("lspconfig")
-
-local cmp = require("cmp")
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-local conform = require("conform")
 
-cmp.setup({
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-  }, {
-    { name = "buffer" },
-  }),
-  window = {
-    completion = cmp.config.window.bordered(),
-	documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-	["<Enter>"] = cmp.mapping.confirm({ select = true }),
-	["<C-Space>"] = cmp.mapping.complete(),
-	["<Tab>"] = nil,
-	["<S-Tab>"] = nil,
-  }),
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end
-  },
-})
 
 vim.api.nvim_create_autocmd("LspAttach", {
   desc = "LSP actions",
---
+++ after/plugin/supermaven.lua
@@ -0,0 +1,8 @@
+local supermaven = require("supermaven-nvim")
+
+-- Thank you Nick for the setup :)
+supermaven.setup({
+    ignore_filetypes = {},
+    disable_inline_completion = true,
+    disable_keymaps = false,
+})
