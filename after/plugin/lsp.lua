f4c6d74f2a4100ba39745c6dceb80cb3076eb478
alecbass <alec.bassingthwaighte@gmail.com>
2025-07-03 09:37:54 +1000

    Change goto type definition to type definition

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -16,7 +16,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-    vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
     vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
     vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
     vim.keymap.set("n", "<leader><F2>", vim.lsp.buf.rename, opts)
