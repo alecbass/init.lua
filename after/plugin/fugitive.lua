98e9ccbf25dee53060f303833bc2e03e5ca3f60d
alecbass <alec.bassingthwaighte@gmail.com>
2025-11-26 10:18:03 +1000

    Toggle fugitive (epic)

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,22 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+function IsFugitiveFile(name)
+    local prefix = "fugitive://"
+    return string.sub(name, 1, #prefix) == prefix
+end
+
+function ToggleFugitive()
+    local was_fugitive_open = false
+
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if IsFugitiveFile(vim.api.nvim_buf_get_name(buf)) then
+            was_fugitive_open = true
+            vim.api.nvim_buf_delete(buf, { force = true })
+            break
+        end
+    end
+
+    if was_fugitive_open == false then
+        vim.cmd.Git()
+    end
+end
+
+vim.keymap.set("n", "<leader>gs", ToggleFugitive)
