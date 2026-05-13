e9cac01b7f0b2e21dbaefbfc87562d96a18a33a0
alecbass <alec.bassingthwaighte@gmail.com>
2026-05-13 14:44:02 +1000

    THE RAZOR LSP FINALLY WORKS AAAAAAAAAAAAAAAA

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -347,50 +347,40 @@ vim.filetype.add({
 	},
 })
 
--- vim.lsp.config("roslyn_ls", {
---     filetypes = { "cs", "razor", "aspnetcorerazor" },
--- 	capabilities = capabilities,
--- 	settings = {
--- 		["csharp|inlay_hints"] = {
--- 			csharp_enable_inlay_hints_for_implicit_object_creation = true,
--- 			csharp_enable_inlay_hints_for_implicit_variable_types = true,
--- 			csharp_enable_inlay_hints_for_lambda_parameter_types = true,
--- 			csharp_enable_inlay_hints_for_types = true,
--- 			dotnet_enable_inlay_hints_for_indexer_parameters = true,
--- 			dotnet_enable_inlay_hints_for_literal_parameters = true,
--- 			dotnet_enable_inlay_hints_for_object_creation_parameters = true,
--- 			dotnet_enable_inlay_hints_for_other_parameters = true,
--- 			dotnet_enable_inlay_hints_for_parameters = true,
--- 			dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
--- 			dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
--- 			dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
--- 		},
--- 		["csharp|code_lens"] = {
--- 			dotnet_enable_references_code_lens = true,
--- 		},
---         ["csharp|completion"] = {
---             dotnet_provide_regex_completions = true,
---             dotnet_show_completion_items_from_unimported_namespaces = true,
---             dotnet_show_name_completion_suggestions = true,
---         }
--- 	},
--- })
--- vim.lsp.enable("roslyn_ls")
+-- TODO(alec): See how the official roslyn-language-server works when it eventually gets .razor support
+require("mason").setup({
+    registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+    },
+})
 
-local cmd = {
-	-- "Microsoft.CodeAnalysis.LanguageServer.dll",
-	"Microsoft.CodeAnalysis.LanguageServer",
-	"--logLevel", -- this property is required by the server
-	"Information",
-	"--extensionLogDirectory", -- this property is required by the server
-	vim.fs.joinpath(vim.uv.os_tmpdir(), "roslyn_ls/logs"),
-	"--stdio",
-}
--- vim.lsp.config("roslyn_ls", {
--- 	cmd = cmd,
---     filetypes = { "cs", "razor", "aspnetcorerazor" },
--- })
--- vim.lsp.enable("roslyn_ls")
+vim.lsp.config("roslyn", {
+	settings = {
+		["csharp|inlay_hints"] = {
+			csharp_enable_inlay_hints_for_implicit_object_creation = true,
+			csharp_enable_inlay_hints_for_implicit_variable_types = true,
+			csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+			csharp_enable_inlay_hints_for_types = true,
+			dotnet_enable_inlay_hints_for_indexer_parameters = true,
+			dotnet_enable_inlay_hints_for_literal_parameters = true,
+			dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+			dotnet_enable_inlay_hints_for_other_parameters = true,
+			dotnet_enable_inlay_hints_for_parameters = true,
+			dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+			dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+			dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+		},
+		["csharp|code_lens"] = {
+			dotnet_enable_references_code_lens = true,
+		},
+        ["csharp|completion"] = {
+            dotnet_provide_regex_completions = true,
+            dotnet_show_completion_items_from_unimported_namespaces = true,
+            dotnet_show_name_completion_suggestions = true,
+        }
+	},
+})
 
 --
 -- Terraform
